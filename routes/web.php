fbcca5a187bf132fcd91dba5f19e78ec0634ba87
Caballero025 <l20590234@sjuanrio.tecnm.mx>
2025-11-07 21:31:59 -0600

    feat/escolares_periodo: agregar funcionalidad para actualizar periodos

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::group(['middleware'=> ['role:escolares']],function(){
     #Rutas de periodos
     Route::get('/periodos',[PeriodoController::class,'index'])->name('Periodos');
     Route::post('/periodos/new',[PeriodoController::class,'createPeriodo'])->name('PeriodosCrear');
+    Route::patch('/periodos/editar/{id}', [PeriodoController::class, 'editarPeriodo'])->name('PeriodosEditar');
+    
+        #Route::patch('/planes-de-estudio/update/{id}',[PlanEstudioController::class,'actualizarPlanEstudio'])->name('PlanesEstudioActualizar');
+
 
 
 });
